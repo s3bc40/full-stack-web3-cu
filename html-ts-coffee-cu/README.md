ad8cc5a301990f43f302137a1ab29645754c383d
s3bc40 <s3bc40@gmail.com>
2025-05-03 19:27:49 +0200

    feat: simulate contract from anvil save state fund

--- html-ts-coffee-cu/README.md
+++ html-ts-coffee-cu/README.md
@@ -1,7 +1,10 @@
 # What are we making?
 - Minimal HTML/JS site
 - That has the following buttons which map to the solidity smart contract:
-  - Connect
+  - Connect OK
   - Buy Coffee
+    - Button to buy coffee
+    - Call a function on a smart contract
+    - Have a test blockchain?
   - Get Balance
   - Withdraw
--- html-ts-coffee-cu/index-js.js
+++ html-ts-coffee-cu/index-js.js
@@ -1,1 +1,75 @@
-console.log("Hello from index.js");
+import { createWalletClient, custom, createPublicClient, defineChain, parseEther } from 'https://esm.sh/viem'
+import { contractAddress, abi } from './constants-js.js';
+
+const connectButton = document.getElementById('connect-button');
+const fundButton = document.getElementById('fund-button');
+const ethAmountInput = document.getElementById('eth-amount');
+
+let walletClient;
+let publicClient;
+
+async function connect() {
+    if (typeof window.ethereum !== 'undefined') {
+        console.log("Connecting using viem...");
+
+        walletClient = createWalletClient({
+            transport: custom(window.ethereum),
+        });
+
+        await walletClient.requestAddresses();
+        connectButton.innerHTML = "Connected to wallet!";
+    } else {
+        connectButton.innerHTML = 'Please install MetaMask!';
+    }
+}
+
+async function fund() {
+    const ethAmount = ethAmountInput.value;
+    console.log(`Funding with ${ethAmount} ETH...`);
+
+    if (typeof window.ethereum !== 'undefined') {
+        walletClient = createWalletClient({
+            transport: custom(window.ethereum),
+        });
+
+        const [connectedAccount] = await walletClient.requestAddresses();
+        const currentChain = await getCurrentChain(walletClient);
+        
+        publicClient = createPublicClient({
+            transport: custom(window.ethereum),
+        });
+
+        await publicClient.simulateContract({
+            address: contractAddress,
+            abi,
+            functionName: 'fund',
+            account: connectedAccount,
+            chain:currentChain,
+            value:parseEther(ethAmount),
+        });
+    } else {
+        connectButton.innerHTML = 'Please install MetaMask!';
+    }
+}
+
+async function getCurrentChain(client) {
+    const chainId = await client.getChainId()
+    const currentChain = defineChain({
+      id: chainId,
+      name: "Custom Chain",
+      nativeCurrency: {
+        name: "Ether",
+        symbol: "ETH",
+        decimals: 18,
+      },
+      rpcUrls: {
+        default: {
+          http: ["http://localhost:8545"],
+        },
+      },
+    })
+    return currentChain
+  }
+
+connectButton.onclick = connect;
+fundButton.onclick = fund;
